0e8e2be8ead41aa5c6149ce614a86bc4c504ab7f
Denis Junior <denis_jr2001@hotmail.com>
2022-07-11 01:28:44 -0300

    file for interfaces and types made

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Person } from './components/Person';
+import { person } from './components/types';
+import { IPerson } from './interface';
 
 function App() {
+  const person: person = {
+    name:"cleiton rasta",
+    age:32
+  }
+
   return (
     <div className="App">
-      oii
+      <Person name={"Denis"} age={20} person={person}/>
     </div>
   );
 }
---
+++ client/src/components/types.ts
@@ -0,0 +1,4 @@
+export type person = {
+    name: string
+    age: number
+}
